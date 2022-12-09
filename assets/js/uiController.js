e84e28db136b1cc750babf9c47656549f61082e7
nico-3p <99618901+nico-3p@users.noreply.github.com>
2022-12-10 03:38:08 +0900

    リアルタイムでリサイズすると重いので、positionをabsolute状態でリサイズするように変更

--- assets/js/uiController.js
+++ assets/js/uiController.js
@@ -1,3 +1,6 @@
+/**
+ * 指定した要素のサイズを変更できるサッシを操作するクラス
+ */
 class SashControl {
     /**
      * @param {document} sash サッシ要素
@@ -12,10 +15,16 @@ class SashControl {
         this.offset = offset;
         this.limit = limit;
 
+        // リサイズ時にデザインが崩れないようにするためのスペーサー
+        this.spacerElement = document.createElement("div");
+        this.spacerElement.style.width = this.content.style.width;
+        this.spacerElement.style.backgroundColor = window.getComputedStyle(this.content, null).getPropertyValue('background-color');
+
         // this.disable();
         this.addEvent();
     }
-    x = 180;
+
+    x = 0;
     sashWidth = 4;
     sashColor = "#007fd4";
     
@@ -54,8 +63,22 @@ class SashControl {
     moveflag = false;
     onMousedown(ts, e) {
         ts.moveflag = true;
-        ts.sash.style.backgroundColor = ts.sashColor;
+
+        // ホバー時スタイルの恒常化
         document.body.style.cursor = "e-resize";
+        ts.sash.style.backgroundColor = ts.sashColor;
+
+        // リアルタイムでリサイズすると重いので、positionをabsolute状態でリサイズ
+        ts.content.style.position = "absolute";
+        ts.content.style.zIndex = "10";
+        ts.content.style.left = ts.offset + "px";
+
+        // spacerElementサイズ更新
+        ts.spacerElement.style.width = ts.content.style.width;
+        ts.spacerElement.style.display = ts.content.style.display;
+
+        // spacerElement挿入
+        ts.content.after(ts.spacerElement);
     }
     onMousemove(ts, e) {
         if (!ts.moveflag) return;
@@ -67,8 +90,18 @@ class SashControl {
         if (!ts.moveflag) return;
 
         ts.moveflag = false;
+
+        // ホバー時スタイルの恒常化 解除
         ts.sash.style.backgroundColor = null;
         document.body.style.cursor = null;
+
+        // absolute 解除
+        ts.content.style.position = null;
+        ts.content.style.zIndex = null;
+        ts.content.style.left = null;
+
+        // spacerElement削除
+        ts.spacerElement.parentNode.removeChild(ts.spacerElement);
     }
 
     onWindowResize(ts, e) {
@@ -80,4 +113,6 @@ class SashControl {
     const sash_sidebar = new SashControl(document.querySelector(".sashContainer .sash_sidebar"),
         document.querySelector(".splitViewContainer .sidebar"),
         48, 180);
+    
+    console.log(sash_sidebar);
 })();
